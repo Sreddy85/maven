b32e6834448c013742b293a8432f8920b7d5504c
sanik <sanik@LAPTOP-2Q9A1CVU>
2020-08-06 15:46:09 +0530

    my commit

---
+++ Actitime/src/test/java/com/actitime/timetrack/MavenTesting.java
@@ -0,0 +1,15 @@
+package com.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MavenTesting {
+	
+	
+	@Test
+	public void run() {
+		Reporter.log("i am run method", true);
+		
+		}
+
+}
